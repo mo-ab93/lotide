4208ec39c1f4090d20e78beb77fcb7a36da0234c
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-25 16:37:10 -0400

    create our own assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 4);
+assertEqual('Mohammed', 'Abbas');
+assertEqual('lighthouse', 'lighthouse');
