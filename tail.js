a9cc4db868c5286c7c3e9545daf86c6fe91fdcf0
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-25 17:42:33 -0400

    add some lint changes on my tail function

--- tail.js
+++ tail.js
@@ -1,18 +1,19 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);  
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
 const tail = function(arr) {
- return arr.slice(1);
-}
+  return arr.slice(1);
+};
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // => will always fail!
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const words2 = tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words2.length, 3); // original array should still have 3 elements!
