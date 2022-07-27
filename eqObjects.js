b33768b7a410a5f2746de1f0b59ef1c0e4915a5c
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-07-27 18:43:24 -0400

    Implement eqObjects in lotide

--- eqObjects.js
+++ eqObjects.js
@@ -39,16 +39,18 @@ return true;
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 // eqObjects(ab, ba);
-console.log(eqObjects(ab, ba))// => true
+// console.log(eqObjects(ab, ba))// => true
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+// console.log(eqObjects(ab, abc)); // => false
+assertEqual(eqObjects(ab, abc), false);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// console.log(eqObjects(cd, dc)); // => true
+assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
-
-
+// eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, cd2), false);
