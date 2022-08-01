4fe9db5c6a404dbd13e31640167ce63b96af2add
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-01 13:28:23 -0400

    Lotide Refactor head and assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);  
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 4);
-assertEqual('Mohammed', 'Abbas');
-assertEqual('lighthouse', 'lighthouse');
+module.exports = assertEqual;
