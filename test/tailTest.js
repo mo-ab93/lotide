7e05a3c64cc04a73d89b05a6f1a40b9ac74fe0f6
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-01 13:35:10 -0400

    Refactor tail in lotide

---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const words2 = tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words2.length, 3); // original array should still have 3 elements!
